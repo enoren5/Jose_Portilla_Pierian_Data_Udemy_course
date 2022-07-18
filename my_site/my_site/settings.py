d078c9c5561cf788ed31f4dccb5dd5b287cce940
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-18 10:14:14 -0400

    S10, L6: Learn template inheritance

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], #Formerly: os.path.join(BASE_DIR, 'templates/')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')], #Formerly: os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
