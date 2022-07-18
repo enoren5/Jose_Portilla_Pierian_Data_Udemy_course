e5f6985996587d20e445eda5c2daefa2c9231ca2
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-18 10:05:19 -0400

    S10,L65 Tags and URL names

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def example_view(request):
-    #my_app/templates/my_app/example.html
-    return render(request, 'my_app/example.html')
-
-def variable_view(request):
-    my_var = {
+my_var = {
         'first_name': 'aleister',
         'last_name': 'crowley',
         'author_list': ['Paul F. Case','Arthur E. Waite','Manly P. Hall','Aleister Crowley',],
@@ -15,4 +10,10 @@ def variable_view(request):
         'user_logged_in': False,
         'some_letter_list': ['a','b','c','d','e','f','g','h','i']
         }
+
+def example_view(request):
+    #my_app/templates/my_app/example.html
+    return render(request, 'my_app/example.html')
+
+def variable_view(request):
     return render(request,'my_app/variable.html', context=my_var)
