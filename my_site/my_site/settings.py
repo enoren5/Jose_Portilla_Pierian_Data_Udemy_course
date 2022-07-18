a0787af3f254cd618f3da54cd816b9abe872a633
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-18 11:28:57 -0400

    S10:L68, Setting up basic static files (.jpg)

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-prf2w^$=cs#blri^8eg0vqw2r16f+_ni#^logg8uw36h0hjm_u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
@@ -116,6 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+# my_app/static/my_app/django.jpg
 
 STATIC_URL = 'static/'
 
