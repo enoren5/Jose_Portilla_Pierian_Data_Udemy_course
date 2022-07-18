938ccc07943325a450cb8ce860a5422b270d14a3
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-18 08:26:18 -0400

    Play with template (Jinja) variables - lists/dicts

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def example_view(request):
     #my_app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+    my_var = {
+        'first_name': 'Aleister',
+        'last_name': 'Crowley',
+        'author_list': ['Paul F. Case','Arthur E. Waite','Manly P. Hall',],
+        'some_dict': {'inside_key':'inside_value'},
+        }
+    return render(request,'my_app/variable.html', context=my_var)
