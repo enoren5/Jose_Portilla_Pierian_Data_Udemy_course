e42949a379844efe86d522a8b3a3b62287ece937
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-18 07:33:20 -0400

    Complete S9:L57 + S10:L58-L59 on Templates

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello World!')
+def example_view(request):
+    #my_app/templates/my_app/example.html
+    return render(request, 'my_app/example.html')
