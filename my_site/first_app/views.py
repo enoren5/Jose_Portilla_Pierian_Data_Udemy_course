54819afe9b504cc5dd36831b254a4a170f51bdd3
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-18 07:13:31 -0400

    Begin Section 10, first_app clean slate

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -2,27 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.urls import reverse
 
-articles = {
-    'sports':'Sports Page',
-    'finance': 'Finance Page',
-    'thelema': '93! “Love is the law, love under will.”',
-}
-
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404("404 Generic") 
-        #return HttpResponseNotFound(result)
-
-def addition_view(request, num1, num2):
-    product = num1 ** num2
-    output = f'{num1} exponent {num2} = {product}' 
-    return HttpResponse(str(output))
-
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]
-    # webpage=reverse('topic-page', args=[topic,])
-    return HttpResponseRedirect(reverse('topic-page', args=[topic,]))
+def simple_view(request):
+    return render(request, 'first_app/example.html') #.html
