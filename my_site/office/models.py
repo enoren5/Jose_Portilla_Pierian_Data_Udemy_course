c77d6ebfee4557c2e4032fd330e378e8275b639a
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-25 04:50:36 -0400

    Add string representation to models

--- my_site/office/models.py
+++ my_site/office/models.py
@@ -6,3 +6,9 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
     # dob = models.DateField() # Proper date of birth
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} is {self.age} years old"
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} is {self.age} years old"
