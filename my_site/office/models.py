09997c9ca08cd923c10e4eb8512d20edc5b79f50
Pete Naveen Pearson <warden496@protonmail.com>
2022-08-01 09:05:06 -0400

    Add dob calculator code

--- my_site/office/models.py
+++ my_site/office/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
-    # dob = models.DateField() # Proper date of birth
+    dob = models.DateField() # Proper more precicse date of birth to be calculated in method below
+    
+    def calculated_age(self):
+        return self.dob - datetime.date(now) 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} is {self.age} years old"
