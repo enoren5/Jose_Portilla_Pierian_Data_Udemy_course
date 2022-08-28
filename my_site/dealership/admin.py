34b97fd59ce881cfbabc6cbda556b76cebb25f55
Pete Naveen Pearson <warden496@protonmail.com>
2022-08-28 15:17:01 -0400

    COMPLETES S12:L86 - Setting up Django Admin with fields

--- my_site/dealership/admin.py
+++ my_site/dealership/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from dealership.models import Vehicle
+
 
 # Register your models here.
+
+class VehicleAdmin(admin.ModelAdmin):
+    # fields = ['year','brand']
+    fieldsets = [
+        ('TIME INFORMATION', {'fields':['year']}),
+        ('VEHICLE INFORMATION', {'fields':['brand']}),
+    ]
+admin.site.register(Vehicle,VehicleAdmin)
