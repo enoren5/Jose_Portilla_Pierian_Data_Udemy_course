145f5241fb149754696a648fe6689c6538093c73
Pete Naveen Pearson <warden496@protonmail.com>
2022-08-28 16:00:35 -0400

    COMPLETES S13:89 on Django Built-In Forms

---
+++ my_site/car_rentals/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ReviewForm(forms.Form):
+    first_name = forms.CharField(label="First Name", max_length=50)
+    last_name = forms.CharField(label="Last Name", max_length=50)
+    email = forms.EmailField(label="Email Address", max_length=50)
+    review = forms.CharField(label="Your comments go here", max_length=5000)
+
--- my_site/car_rentals/views.py
+++ my_site/car_rentals/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from . import models
+from .forms import ReviewForm
 
 def rental_review(request):
-    return render(request, 'car_rentals/rental_review.html')
+    
+    # POST REQUEST -- > CONTENTS --> THANKYOU
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        
+        # VALIDATE ROUTINE::
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect(reverse('car_rentals:thank_you'))
+    # ELSE, Re-RENDER FORM
+    else:
+        form = ReviewForm
+    return render(request, 'car_rentals/rental_review.html', context = {'form':form})
 
 def thank_you(request):
     return render(request, 'car_rentals/thank_you.html')
