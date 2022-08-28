692f9ba00ed3410786b32235bf8ba5192764d4a2
Pete Naveen Pearson <warden496@protonmail.com>
2022-08-28 15:06:51 -0400

    COMPLETES S12:L84 - Add,Delete,List functionality

--- my_site/dealership/views.py
+++ my_site/dealership/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 
 def list_view(request):
-    return render(request, 'dealership/list.html')
-
-def delete(request):
-    return render(request, 'dealership/delete.html')
+    all_vehicles = models.Vehicle.objects.all()
+    context = {'vehicles':all_vehicles}
+    return render(request, 'dealership/list.html', context=context)
 
 def add(request):
-    return render(request, 'dealership/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Vehicle.objects.create(brand=brand, year=year)
+        # After users submits car, redirect to list.html
+        return redirect(reverse('dealership:list'))
+    else:
+        return render(request, 'dealership/add.html')
+    
+def delete(request):
+    if request.POST:
+        # delete the card
+        pk = request.POST['pk']
+        try:
+            models.Vehicle.objects.get(pk=pk).delete()
+            return redirect(reverse('vehicles:list'))
+        except:
+            print('pk not found')
+            return redirect(reverse('dealership:list'))
+    else:
+        return render(request, 'dealership/delete.html')
