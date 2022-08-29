5e139f92dcc7d373024454ffe56bc30e5fc3ba19
Pete Naveen Pearson <warden496@protonmail.com>
2022-08-28 21:46:15 -0400

    COMPLETES S13:L91 on Django Forms widget styling

--- my_site/car_rentals/forms.py
+++ my_site/car_rentals/forms.py
@@ -4,5 +4,9 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label="First Name", max_length=50)
     last_name = forms.CharField(label="Last Name", max_length=50)
     email = forms.EmailField(label="Email Address", max_length=50)
-    review = forms.CharField(label="Your comments go here", max_length=5000)
+    review = forms.CharField(
+        label="Your comments go here", 
+        max_length=500,
+        widget=forms.Textarea(attrs={'class':'myform', 'rows':'12','cols':'25'})
+        )
 
